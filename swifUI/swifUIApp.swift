fe92a1969335bf6d09d060dba95d4fb424f64799
Yash <mayurbakraniya165@gmail.com>
2021-04-15 14:28:54 +0530

    Initial Commit

---
+++ swifUI/swifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifUIApp.swift
+//  swifUI
+//
+//  Created by Adsum MAC 3 on 15/04/21.
+//
+
+import SwiftUI
+
+@main
+struct swifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
