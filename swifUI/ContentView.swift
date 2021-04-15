15983164420a5a20e6f63914d6cb6644375c00b6
Yash <mayurbakraniya165@gmail.com>
2021-04-15 16:04:06 +0530

    15/04/2021

--- swifUI/ContentView.swift
+++ swifUI/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var checkAmount = ""
+    @State var noOfPeople = 2
+    @State var tipPercentage = 2
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPeron : Double{
+        let peopelCount = Double(noOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopelCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount).keyboardType(.decimalPad)
+                
+                    Picker("Number of People", selection: $noOfPeople){
+                        ForEach(2..<100){
+                            Text("People : \($0)")
+                        }
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave? ")){
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(tipPercentages[$0])%")
+                        }
+
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+    
+                Section(header: Text("Your split Amount")){
+                    Text("Your Split Amount :  ₹\(totalPerPeron,specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("Amount",displayMode: .large)
+        }
     }
 }
 
